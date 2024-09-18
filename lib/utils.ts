3ff3e6954df51541cc8f0834d393e3e331679a0b
TheSoftNode <thesoftnode@gmail.com>
2024-09-18 21:47:27 +0100

    updated the packages and utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,37 +11,52 @@ export function cn(...inputs: ClassValue[]) {
 // FORMAT DATE TIME
 export const formatDateTime = (dateString: Date) => {
   const dateTimeOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    day: "numeric", // numeric day of the month (e.g., '25')
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
   const dateDayOptions: Intl.DateTimeFormatOptions = {
-    weekday: "short",
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
+    weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+    year: "numeric", // numeric year (e.g., '2023')
+    month: "2-digit", // abbreviated month name (e.g., 'Oct')
+    day: "2-digit", // numeric day of the month (e.g., '25')
   };
 
   const dateOptions: Intl.DateTimeFormatOptions = {
-    month: "short",
-    year: "numeric",
-    day: "numeric",
+    month: "short", // abbreviated month name (e.g., 'Oct')
+    year: "numeric", // numeric year (e.g., '2023')
+    day: "numeric", // numeric day of the month (e.g., '25')
   };
 
   const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
+    hour: "numeric", // numeric hour (e.g., '8')
+    minute: "numeric", // numeric minute (e.g., '30')
+    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
 
-  const formattedDateTime = new Date(dateString).toLocaleString("en-US", dateTimeOptions);
-  const formattedDateDay = new Date(dateString).toLocaleString("en-US", dateDayOptions);
-  const formattedDate = new Date(dateString).toLocaleString("en-US", dateOptions);
-  const formattedTime = new Date(dateString).toLocaleString("en-US", timeOptions);
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
+
+  const formattedDateDay: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateDayOptions
+  );
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    timeOptions
+  );
 
   return {
     dateTime: formattedDateTime,
@@ -75,6 +90,7 @@ interface UrlQueryParams {
 
 export function formUrlQuery({ params, key, value }: UrlQueryParams) {
   const currentUrl = qs.parse(params);
+
   currentUrl[key] = value;
 
   return qs.stringifyUrl(
@@ -85,8 +101,6 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
     { skipNull: true }
   );
 }
-
-type AccountTypes = "depository" | "credit" | "other"; // Adjust as needed
 
 export function getAccountTypeColors(type: AccountTypes) {
   switch (type) {
@@ -116,41 +130,53 @@ export function getAccountTypeColors(type: AccountTypes) {
   }
 }
 
-type Transaction = {
-  category: string;
-};
-
-type CategoryCount = {
-  name: string;
-  count: number;
-  totalCount: number;
-};
-
-export function countTransactionCategories(transactions: Transaction[]): CategoryCount[] {
+export function countTransactionCategories(
+  transactions: Transaction[]
+): CategoryCount[] {
   const categoryCounts: { [category: string]: number } = {};
   let totalCount = 0;
 
-  transactions.forEach((transaction) => {
-    const category = transaction.category;
+  // Iterate over each transaction
+  transactions &&
+    transactions.forEach((transaction) => {
+      // Extract the category from the transaction
+      const category = transaction.category;
 
-    categoryCounts[category] = (categoryCounts[category] || 0) + 1;
-    totalCount++;
-  });
+      // If the category exists in the categoryCounts object, increment its count
+      if (categoryCounts.hasOwnProperty(category)) {
+        categoryCounts[category]++;
+      } else {
+        // Otherwise, initialize the count to 1
+        categoryCounts[category] = 1;
+      }
 
-  const aggregatedCategories = Object.keys(categoryCounts).map((category) => ({
-    name: category,
-    count: categoryCounts[category],
-    totalCount,
-  }));
+      // Increment total count
+      totalCount++;
+    });
 
+  // Convert the categoryCounts object to an array of objects
+  const aggregatedCategories: CategoryCount[] = Object.keys(categoryCounts).map(
+    (category) => ({
+      name: category,
+      count: categoryCounts[category],
+      totalCount,
+    })
+  );
+
+  // Sort the aggregatedCategories array by count in descending order
   aggregatedCategories.sort((a, b) => b.count - a.count);
 
   return aggregatedCategories;
 }
 
 export function extractCustomerIdFromUrl(url: string) {
+  // Split the URL string by '/'
   const parts = url.split("/");
-  return parts[parts.length - 1];
+
+  // Extract the last part, which represents the customer ID
+  const customerId = parts[parts.length - 1];
+
+  return customerId;
 }
 
 export function encryptId(id: string) {
@@ -170,6 +196,7 @@ export const getTransactionStatus = (date: Date) => {
 };
 
 export const authFormSchema = (type: string) => z.object({
+  // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
@@ -178,6 +205,7 @@ export const authFormSchema = (type: string) => z.object({
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // both
   email: z.string().email(),
   password: z.string().min(8),
-});
+})
